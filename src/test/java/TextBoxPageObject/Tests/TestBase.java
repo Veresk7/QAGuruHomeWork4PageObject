944aff768f7d6a5f95d7184f478188b15f3884d1
Sergey Danilov-Skobelev <veresk81@yandex.ru>
2026-02-27 21:55:26 +0400

    Browser window is closing after test cycle

--- src/test/java/TextBoxPageObject/Tests/TestBase.java
+++ src/test/java/TextBoxPageObject/Tests/TestBase.java
@@ -13,7 +13,7 @@ public class TestBase {
         Configuration.browser = "chrome";
         Configuration.browserSize = "1920x1080";
         Configuration.pageLoadStrategy = "eager";
-        Configuration.holdBrowserOpen = true;
+        Configuration.holdBrowserOpen = false;
     }
 
     @AfterEach
